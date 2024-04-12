845912f4a0859d7a5e47d99f651f3ea2e5353130
himanshu.kotnala <himanshu.kotnala@tafensw.edu.au>
2024-04-12 17:22:29 +1000

    new layout changes

--- resources/views/layouts/empirical.blade.php
+++ resources/views/layouts/empirical.blade.php
@@ -22,7 +22,7 @@
 
     <link rel="stylesheet" href="{{ asset('storage/css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('storage/css/icomoon.css') }}">
-    <link rel="stylesheet" href="{{ asset('storage/css/style.css?v=1.3') }}">
+    <link rel="stylesheet" href="{{ asset('storage/css/style.css?v=1.4') }}">
 </head>
 <body>
 
